072b976176c4ad1155b15c268923a2db8c49bcdc
pc215 <chouksepranav@gmail.com>
2023-07-21 20:23:42 +0100

    Add header files with declarations

---
+++ phases.h
@@ -0,0 +1,5 @@
+extern char *passwd;
+extern void explode_bomb( void );
+extern void phase_1(char *input);
+extern void phase_2(char *input);
+extern void phase_3(char *input);
---
+++ support.h
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+extern void initialise_bomb(char *name);
+extern char *read_input(FILE *in_fp);
+extern void phase_defused( void );
